2674fbf73289121cb5d9dfe4f710fa805efcff0f
Dayan Kodippily <dkodippily@gmail.com>
2020-06-13 23:51:23 +1200

    added eureka service registration

--- propwand/propwand-user-service/src/main/java/com/thoughtwand/propwand/userservice/PropwandUserServiceApplication.java
+++ propwand/propwand-user-service/src/main/java/com/thoughtwand/propwand/userservice/PropwandUserServiceApplication.java
@@ -2,8 +2,10 @@ package com.thoughtwand.propwand.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PropwandUserServiceApplication {
 
     public static void main(String[] args) {
